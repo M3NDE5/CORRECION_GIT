d640b18a48a40af331f217671e52637d8076e701
Juan Mendez <juanangelmendez9@gmail.com>
2025-01-30 20:51:16 -0500

    registros actualizados

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ from registro import *
 
 opc = 0
 
-while opc != 1:
-    print("MENU\n1. RESGISTRO MAESTROS")
+while opc != 3:
+    print("MENU\n1. RESGISTRO MAESTROS\n2. REGISTRO ESTUDIANTES")
     opc = int(input("Ingrese una opcion: "))
     match opc:
         case 1:
-            print("REGISTRO\n")
+            print("REGISTRO ADMINISTRATIVOS\n")
             registro_profes_admins()
+        case 2:
+            print("REGISTRO ESTUDIANTES")
+            registro_estudiantes()
